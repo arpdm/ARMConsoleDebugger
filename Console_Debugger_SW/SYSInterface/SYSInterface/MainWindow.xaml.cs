59e3b8dd2bb0d28152cb5e64fb127e60e636b872
arpdm <arpiderm@gmail.com>
2018-01-11 15:35:16 -0800

    Added C# System Interface Software

---
+++ Console_Debugger_SW/SYSInterface/SYSInterface/MainWindow.xaml.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SYSInterface
+{
+    public partial class MainWindow : Window
+    {
+
+        private SerialLib serialLib = new SerialLib();
+
+        /*****************************************************************************
+        * Function :   MainWindow
+        * Input    :   none
+        * Output   :   none
+        * Comment  :   main function runs on load
+        ****************************************************************************/
+
+        public MainWindow()
+        {
+
+            InitializeComponent();
+
+            //Set the current system time
+            setTimeStamp();
+
+            //Configure all UI Objects
+            configureCommPortComboBox();
+            configureBaudRateComboBox();
+            configureParityComboBox();
+            configureDataBitsComboBox();
+            configureStopBitsComboBox();
+            configureHandshakeComboBox();
+
+        }
+
+        /*****************************************************************************
+         * Function :   setTimeStamp
+         * Input    :   none
+         * Output   :   none
+         * Comment  :   Gets the current system date time
+         ****************************************************************************/
+
+        private void setTimeStamp()
+        {
+
+            timeStampTxt.Text = DateTime.Now.ToString();
+
+        }
+
+        /*****************************************************************************
+         * Function :   configureCommPortComboBox
+         * Input    :   none
+         * Output   :   none
+         * Comment  :   
+         ****************************************************************************/
+
+        private void configureCommPortComboBox()
+        {
+
+            String[] commPorts = this.serialLib.GetCommPorts();
+
+            //First clear all the items before re populating the dropdown
+            comPortComboBox.Items.Clear();
+
+            //Add all detectected comm ports to the combo box
+            foreach (string port in commPorts)
+            {
+
+                comPortComboBox.Items.Add(port);
+
+            }
+        }
+
+        /*****************************************************************************
+         * Function :   configureBaudRateComboBox
+         * Input    :   none
+         * Output   :   none
+         * Comment  :   
+         ****************************************************************************/
+
+        private void configureBaudRateComboBox()
+        {
+
+            baudRateComboBox.Items.Clear();
+            baudRateComboBox.Items.Add(1200);
+            baudRateComboBox.Items.Add(2400);
+            baudRateComboBox.Items.Add(4800);
+            baudRateComboBox.Items.Add(9600);
+            baudRateComboBox.Items.Add(19200);
+            baudRateComboBox.Items.Add(38400);
+            baudRateComboBox.Items.Add(57600);
+            baudRateComboBox.Items.Add(115200);
+
+        }
+
+        /*****************************************************************************
+         * Function :   configureParityComboBox
+         * Input    :   none
+         * Output   :   none
+         * Comment  :   
+         ****************************************************************************/
+
+        private void configureParityComboBox()
+        {
+
+            parityComboBox.Items.Clear();
+            parityComboBox.Items.Add("None");
+            parityComboBox.Items.Add("Even");
+            parityComboBox.Items.Add("Odd");
+
+        }
+
+        /*****************************************************************************
+         * Function :   configureDataBitsComboBox
+         * Input    :   none
+         * Output   :   none
+         * Comment  :   
+         ****************************************************************************/
+
+        private void configureDataBitsComboBox()
+        {
+
+            dataBitsComboBox.Items.Clear();
+            dataBitsComboBox.Items.Add("8 bits");
+            dataBitsComboBox.Items.Add("7 bits");
+
+        }
+
+        /*****************************************************************************
+         * Function :   configureStopBitsComboBox
+         * Input    :   none
+         * Output   :   none
+         * Comment  :   
+         ****************************************************************************/
+
+        private void configureStopBitsComboBox()
+        {
+
+            stopBitsComboBox.Items.Clear();
+            stopBitsComboBox.Items.Add("1 bit");
+            stopBitsComboBox.Items.Add("2 bits");
+
+        }
+
+        /*****************************************************************************
+         * Function :   configureHandshakeComboBox
+         * Input    :   none
+         * Output   :   none
+         * Comment  :   
+         ****************************************************************************/
+
+        private void configureHandshakeComboBox()
+        {
+
+            handShakeComboBox.Items.Clear();
+            handShakeComboBox.Items.Add("None");
+            handShakeComboBox.Items.Add("RTS/CTS");
+
+        }
+
+        /*****************************************************************************
+         * Function :   openCommBtn_Click
+         * Input    :   none
+         * Output   :   none
+         * Comment  :   
+         ****************************************************************************/
+
+        private void openCommBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+
+            //TODO: Initialize SerialLib
+            //TODO: Open Connection
+            //TODO: Clear the buffer
+
+        }
+
+        /*****************************************************************************
+         * Function :   closeCommBtn_Click
+         * Input    :   none
+         * Output   :   none
+         * Comment  :   
+         ****************************************************************************/
+
+        private void closeCommBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+
+            //TODO: Clear the buffer
+            //TODO: Close the SerialLib communication
+
+        }
+
+        /*****************************************************************************
+         * Function :   clearBufferBtn_Click
+         * Input    :   none
+         * Output   :   none
+         * Comment  :   
+         ****************************************************************************/
+
+        private void clearBufferBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+
+        }
+    }
+}
---
+++ Console_Debugger_SW/SYSInterface/SYSInterface/SerialLib.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.IO.Ports;
+using System.Text;
+using System.Threading;
+
+public class SerialLib
+{
+
+
+    private static SerialPort serialPort = new SerialPort();
+
+    //Serial Comm Configuration
+
+    private int baudRate = 115200;
+    private int dataBits = 8;
+    private Parity parity = Parity.None;
+    private string portName = "COM3";
+    private StopBits stopBits = StopBits.One;
+    private Handshake handshake = Handshake.None;
+
+    //Rx Buffer Parameters
+
+    private int rxTimeout = 500;
+    static bool rxEnabled = false;
+
+    //Multi Threading
+
+    Thread rxThread = new Thread(rxData);
+
+    /*****************************************************************************
+     * Function :   GetCommPorts
+     * Input    :   none
+     * Output   :   List of detected comm ports
+     * Comment  :   Gets all the comm ports detected from device manager
+     ****************************************************************************/
+
+    public string[] GetCommPorts()
+    {
+
+        string[] commPorts = SerialPort.GetPortNames();
+        Console.WriteLine(SerialPort.GetPortNames());
+        return commPorts;
+
+    }
+
+    /*****************************************************************************
+     * Function :   OpenSerialConnection
+     * Input    :   none
+     * Output   :   none
+     * Comment  :   Opens a new serial communication
+     ****************************************************************************/
+
+    public void Initialize(int baudRate, int dataBits, string portName, Parity parity, StopBits stopBits, Handshake handshake)
+    {
+
+        this.baudRate = baudRate;
+        this.dataBits = dataBits;
+        this.portName = portName;
+        this.parity = parity;
+        this.stopBits = stopBits;
+        this.handshake = handshake;
+
+    }
+
+    /*****************************************************************************
+     * Function :   OpenSerialConnection
+     * Input    :   none
+     * Output   :   none
+     * Comment  :   Opens a new serial communication
+     ****************************************************************************/
+
+    public bool OpenSerialConnection()
+    {
+
+        try
+        {
+
+            serialPort.BaudRate = this.baudRate;
+            serialPort.DataBits = this.dataBits;
+            serialPort.Handshake = this.handshake;
+            serialPort.Parity = this.parity;
+            serialPort.PortName = this.portName;
+            serialPort.StopBits = this.stopBits;
+            serialPort.ReadTimeout = this.rxTimeout;
+
+
+        }
+        catch
+        {
+
+            return false;
+
+        }
+
+        //Open the serial port
+        serialPort.Open();
+
+        //Start the thread for reading data
+        rxThread.Start();
+
+        return true;
+
+    }
+
+    /*****************************************************************************
+     * Function :   close
+     * Input    :   none
+     * Output   :   none
+     * Comment  :   Close the serial connection
+     ****************************************************************************/
+
+    public void close()
+    {
+
+
+    }
+
+    /*****************************************************************************
+     * Function :   rxData
+     * Input    :   none
+     * Output   :   none
+     * Comment  :   Reads individual bytes recieved from Peripheral device
+     ****************************************************************************/
+
+    public static void rxData()
+    {
+
+        while (rxEnabled)
+        {
+
+            try
+            {
+
+                int rxByte = serialPort.ReadByte();
+                Console.WriteLine(rxByte);
+
+            }
+            catch (TimeoutException)
+            {
+
+
+            }
+        }
+
+    }
+
+}
