db0601e53380cd94b7d3dabdfc417dd6d287a090
arpdm <arpiderm@gmail.com>
2018-01-11 11:31:55 -0800

    Added Python Serial Code
    This code can communicate with the embedded system and read data output for debugging purposes

--- py_serial_interface.py
+++ py_serial_interface.py
@@ -1,11 +1,38 @@
+## Python UART/Serial Source Code
+## This code can be used for interfacing with embedded system using UART/USB cable
+## the serial comm configuration parameters need to match the parameters set on Embedded System
+## This code is bare minimum. Other features can be developed on top of it
+
 import serial
 import time
+import io
 
-ser = serial.Serial('COM3',9600,timeout=10)
+## Configuration Parameters
+
+COM_PORT  = 'COM3'
+BAUD_RATE = 9600
+TIME_OUT  = 10
+
+## Create Instance Of Serial Object
+
+serialObj = serial.Serial(COM_PORT,BAUD_RATE,timeout=TIME_OUT)
 
 while 1:
+
+	## Try reading data
+
 	try:
-		print ser.readline()
-	except ser.SerialTimeoutException:
+	
+		data = serialObj.readline()
+		print (data)
+
+	## Handle execptions
+
+	except serialObj.SerialTimeoutException:
+
 		print("Data could not be read")
 	
+
+
+def closePort():
+	serialObj.close()
