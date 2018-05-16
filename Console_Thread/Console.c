a2a86ca3354105abd6d62a0912bc739dc626bf3d
Arpi Derm <arpi.dermardirousian@wetdesign.com>
2018-05-16 12:54:53 -0700

    Updated console thread

--- Console_Thread/Console.c
+++ Console_Thread/Console.c
@@ -6,10 +6,10 @@
   * @brief   Source file of Console Module.
   ******************************************************************************
   *
-  * @comments
-  *
+  * @Notes
   *
   *****************************************************************************/
+
 
 /* Includes ------------------------------------------------------------------*/
 
@@ -25,36 +25,109 @@
 
 /* Defines -------------------------------------------------------------------*/
 
-UART_HandleTypeDef     huart2;       //UART Driver Object Instance
-osThreadId             tid_Console;  //Console Thread ID 
+extern UART_HandleTypeDef     huart2;               //UART Driver Object Instance
+osThreadId                    tid_Console;          //Console Thread ID 
+uint8_t                       RXDataCount;
+uint8_t                       RXBuffer[20];         //Debug Console RX Buffer  
+char                          TXBuffer[20];         //Debug Console Transmit buffer
+uint8_t                       rxCounter;            //Internal RX Counter
+uint8_t                       recieved;             //RX Recieved Char
 
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart);
+
+
+/* Console Commands Table ----------------------------------------------------*/
+
+    
+/*****************************************************************************
+ * Function :   Console_Send_Data  
+ * Input    :   none
+ * Output   :   none
+ * Comment  :   Send data to console
+ *
+ *              Implementation Example:
+ *              double value = 1.59;
+                sprintf(TXBuffer,"%f\r\n",value);
+                Console_Send_Data(TXBuffer,sizeof(TXBuffer));
+ ****************************************************************************/
+
+void Console_Send_Data(char *txBuffer, uint8_t size){
+    
+    HAL_UART_Transmit_IT(&huart2,(uint8_t *)txBuffer,size);
+    
+}
 
 /*****************************************************************************
- * Function :   asciiToInt  
- * Input    :   none
- * Output   :   converted 16 bit integer value
- * Comment  :   Converts the asccii input from user to 16 bit integer value
+ * Function :   HAL_UART_RxCpltCallback  
+ * Input    :   huart2 instance
+ * Output   :   none
+ * Comment  :   UART Interrupt RX Function
  ****************************************************************************/
- 
-uint16_t asciiToInt(){
+
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart){
+
+    HAL_UART_Receive_IT(&huart2,&recieved,1);
     
-    uint16_t currentNumber = 0;
-    uint16_t result = 0;
-    uint16_t totalNumbers =  RXDataCount - 1;
-    uint16_t power = totalNumbers;
+    //On interrupt if received data is not 0 or CR, append to the RX Buffer
     
-    for(uint8_t i = 0; i < totalNumbers  ; i++){
+    if(recieved > 0 && recieved != 0x0D){
         
-        currentNumber = RXBuffer[i+1];
-        power--;
-        result += (currentNumber-0x30)*pow(10,power);
+       RXBuffer[rxCounter] = recieved;
+       rxCounter++;
         
     }
     
-    return result;
-
+    //If the the CR is transmitted, it means it is the end of packet
+    //At end of each packet, we want to send a signal to console thread to wake it up
+    
+    if(recieved==0x0D){
+        
+        recieved = 0x00;
+        RXDataCount = rxCounter;
+        
+        //Send the signal when user sends CR to complete the packet
+        osSignalSet(tid_Console,0x0001);
+        
+        //Reset the RX counter and RX Buffer
+        rxCounter = 0; 
+        
+    }  
 }
 
+
+/*****************************************************************************
+ * Function :   ParserStateMachine  
+ * Input    :   none
+ * Output   :   none
+ * Comment  :   This function parses user inputs and statemachine decides
+ *              what to do with individual inputs
+ ****************************************************************************/
+ 
+void ParserStateMachine(){
+ 
+    uint8_t command = RXBuffer[0];
+          
+    switch (command){
+         
+        case 0x31:
+             
+            sprintf(TXBuffer,"Systen In Console Mode\r\n");
+            Console_Send_Data(TXBuffer,sizeof(TXBuffer));
+            break;
+        
+        case 0x30:
+            
+            printf("System Status. Console Thread running\r");
+            break;
+         
+        default:
+             
+            printf("Invalid Command\r");
+            break;
+     
+     }  
+ }
+ 
 /*****************************************************************************
  * Function :   ConsoleThread  
  * Input    :   optional parameter passed into the thread
@@ -63,15 +136,18 @@ uint16_t asciiToInt(){
  ****************************************************************************/
 
 void ConsoleThread (void const *argument){
-        
+    
+    //Enable UART RX Interrupt
+    HAL_UART_Receive_IT(&huart2,&recieved,1);
+   
     while(1){
     
-        //Wait for 0x01 signal from UART1 Interrupt Handler
+        //Wait for 0x01 signal from UART Interrupt Handler
         osSignalWait(0x0001,osWaitForever);
 
-        uint8_t command = RXBuffer[0];
-        //TODO: Add state machine or any other operation 
-    
+        ParserStateMachine();
+        osDelay(5);     
+
     }
 }
 
@@ -79,21 +155,21 @@ void ConsoleThread (void const *argument){
  * Function :   initConsoleThread  
  * Input    :   none
  * Output   :   State
- * Comment  :   Initialize the Console Thread
+ * Comment  :   Initialize the Console Thread. 
  ****************************************************************************/
 
 int initConsoleThread(void){
-  
+
     //Define console thread object
-    osThreadDef(consoleThread,ConsoleThread, osPriorityNormal, 0, 128); 
+    osThreadDef(consoleThread,ConsoleThread, osPriorityNormal, 0, 200); 
     
     //Create console thread
     tid_Console = osThreadCreate(osThread(consoleThread), NULL);
  
     //Enable UART Interrupt
-    //NOTE: This can change based on toolchain and MCU used
     __HAL_UART_ENABLE_IT(&huart2, UART_IT_TC);
 
+    //If Console thread did not get initialized, return -1
     if (!tid_Console) return(-1);
    
     return(0);
--- Console_Thread/Console.h
+++ Console_Thread/Console.h
@@ -7,9 +7,12 @@
   ******************************************************************************
   * @attention
   *
+  * <h2><center>&copy; COPYRIGHT(c) 2017 Company Name</center></h2>
+  * 
+  *
   * @Configuration
   *
-  * Baud Rate: 9600
+  * Baud Rate: 115200
   * Parity:    None
   * Data Bits: 8
   * Stop Bits: 1
@@ -24,9 +27,8 @@
 
 /* Includes ------------------------------------------------------------------*/
 
-#include "stm32f0xx_hal.h"
+#include "stm32f4xx_hal.h"
 #include "cmsis_os.h" 
-#include "osObjects.h" 
 
 /* Definitions ---------------------------------------------------------------*/
 
@@ -38,7 +40,7 @@
  * Function :   initConsoleThread  
  * Input    :   none
  * Output   :   State
- * Comment  :  	Initialize the Console Thread
+ * Comment  :   Initialize the Console Thread
  ****************************************************************************/
  
 int initConsoleThread(void);
